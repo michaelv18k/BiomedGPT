05cf77cf7313ca822fdb72c8caeb4bb56d393112
Michael <vmichael3690@gmail.com>
2025-06-04 23:00:43 +0530

    update

--- fairseq/fairseq/data/encoders/gpt2_bpe.py
+++ fairseq/fairseq/data/encoders/gpt2_bpe.py
@@ -29,7 +29,8 @@ class GPT2BPEConfig(FairseqDataclass):
 @register_bpe("gpt2", dataclass=GPT2BPEConfig)
 class GPT2BPE(object):
     def __init__(self, cfg):
-        encoder_json = file_utils.cached_path(cfg.gpt2_encoder_json)
+        # encoder_json = file_utils.cached_path(cfg.gpt2_encoder_json)
+        encoder_json = '/content/BiomedGPT/utils/BPE/encoder.json'
         vocab_bpe = file_utils.cached_path(cfg.gpt2_vocab_bpe)
         self.bpe = get_encoder(encoder_json, vocab_bpe)
 
